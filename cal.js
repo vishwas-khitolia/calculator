1601da761094ecdb3dfd3fcb4237cde552f3b975
vishwas-khitolia <vishwaskhitolia@gmail.com>
2024-03-21 12:32:36 +0530

    git remote add origin https://github.com/vishwas-khitolia/calculator.git git branch -M main git push -u origin main

--- cal.js
+++ cal.js
@@ -1,15 +1,20 @@
 let Result = document.getElementById("result");
 
+// insertValue Function
 function insertValue(value) {
   Result.value += value;
 }
+
+// Clear Function
 function clearResult() {
   Result.value = "";
 }
 
+// Delet Function
 function deletResult() {
   Result.value = Result.value.slice(0, -1);
 }
+// Calculation
 function calculat() {
   try {
     Result.value = eval(Result.value);
